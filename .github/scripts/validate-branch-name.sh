01d2679f28f3b0ca77a2022a6a7b1597f24a8758
krish5152 <100112282+krish5152@users.noreply.github.com>
2024-09-30 17:18:04 +0530

    Update validate-branch-name.sh

--- .github/scripts/validate-branch-name.sh
+++ .github/scripts/validate-branch-name.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+branch_name=$(git rev-parse --abbrev-ref HEAD)
+
+if ! [[ "$branch_name" =~ ^(feature|bugfix|hotfix|release)/.+$ ]]; then
+  echo "Invalid branch name. Branch name must start with feature/, bugfix/, hotfix/, or release/."
+  exit 1
+fi
+
+echo "Branch name is valid."
